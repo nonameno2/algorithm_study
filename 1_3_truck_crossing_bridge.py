92c9d1e9386280d64293a4e9d7072d4cd9eaf5a5
nonameno2 <88826228+nonameno2@users.noreply.github.com>
2021-08-30 09:35:04 +0900

    Update 1_3_truck_crossing_bridge.py

--- 1_3_truck_crossing_bridge.py
+++ 1_3_truck_crossing_bridge.py
@@ -1,3 +1,5 @@
+# https://programmers.co.kr/learn/courses/30/lessons/42583
+
 def solution(bridge_length, weight, truck_weights):
     
     answer = 0                                              # the time it takes for all trucks to cross the bridge, zero initialization
